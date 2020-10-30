aae0c0a3f2992f5201c50264a0e6987937547b8e
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2020-10-30 12:34:22 +0100

    Fehlerechnung fast vollständig

--- waermepumpe.py
+++ waermepumpe.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sympy
 from sympy import *
+from uncertainties import ufloat
 
 t, T1, p1, T2, p2, N = np.genfromtxt('Waermepumpe.txt', unpack=True)
 
@@ -22,13 +23,22 @@ params2, covariance_matrix2 = np.polyfit(t, T2, deg=2, cov=True)
 #Unsicherheit der Regression
 errors1 = np.sqrt(np.diag(covariance_matrix1))
 errors2 = np.sqrt(np.diag(covariance_matrix2))
+h=ufloat(0,0)
+paramserr1=np.array([h, h, h]) #ufloatarrays um die Fehler zu speichern
+paramserr2=np.array([h, h, h])
+i=0
+j=0
 print("Die Parameter der Regression für T1: \n")
 for name, value, error in zip('abc', params1, errors1):
+    paramserr1[i]=ufloat(value, error)
     print(f'{name} = {value:.8f} ± {error:.8f}')
+    i=i+1
 print("\n")
 print("Die Parameter der Regression für T2: \n")
 for name, value, error in zip('abc', params2, errors2):
+    paramserr2[j]=ufloat(value, error)
     print(f'{name} = {value:.8f} ± {error:.8f}')
+    j=j+1
 print("\n")
 
 #Plotten der Regression
@@ -54,16 +64,15 @@ plt.xlabel(r"$t/s$")
 plt.ylabel(r"$T/K$")
 plt.legend(loc="best")
 plt.savefig('Temperaturverlaeufe.pdf')
-
 ############################################################################################
+
 #c, d) Differentailquotient und Güteziffer
 
 #Berechnung von dT/dt
-x = sympy.var('x')
-funkT1=params1[0] * (x)**2 + params1[1] * x + params1[2]
-funkT2=params2[0] * (x)**2 + params2[1] * x + params2[2]
-difT1=funkT1.diff(x) #dT1/dt
-difT2=funkT2.diff(x) #dT2/dt
+def difT1(x):
+    return(2*paramserr1[0]*x+paramserr1[1])#Die Berechnung der quadratischen Funktion erfolgte per Hand
+def difT2(x):
+    return(2*paramserr2[0]*x+paramserr2[1])
 
 #Berechnung der Güteziffern
 i=1
@@ -72,10 +81,10 @@ a2=750
 print("Berechnet mit T1 \n\n")
 while(i<40): #4 unterschiedliche Zeiten mit 10s Abstand
     vid=T1[i]/(T1[i]-T2[i]) #ideale Güte
-    vreal1=((a1+a2)*difT1.subs(x, i))/N[i] #Reale Güte
+    vreal1=((a1+a2)*difT1(i)/N[i]) #Reale Güte
     p=(vid-vreal1)/vid #Berechnet die Abweichung zwischen vreal und vid in %
-    print(f"die reale  Güte nach t={t[i]} Sekunden {vreal1:.5f}")
-    print(f"die ideale Güte nach t={t[i]} Sekunden {vid:.5f}")
+    print(f"die reale  Güte nach t={t[i]}s: v_re={vreal1:.3f}")
+    print(f"die ideale Güte nach t={t[i]}s: v_id={vid:.3f}")
     print(f"die Abweichung beträgt {p*100:.2f}% vom Idealwert")
     print("\n")
     i=i+10
@@ -83,10 +92,10 @@ i=1
 print("Berechnet mit T2 \n\n") #!!!!!!!!!!nochmal überprüfen!!!!!!!!!
 while(i<40): #4 unterschiedliche Zeiten mit 10s Abstand
     vid=-T2[i]/(T2[i]-T1[i]) #ideale Güte
-    vreal2=-((a1+a2)*difT2.subs(x, i))/N[i] #Reale Güte
+    vreal2=-((a1+a2)*difT2(i)/N[i]) #Reale Güte
     p=(vid-vreal2)/vid #Berechnet die Abweichung zwischen vreal und vid in %
-    print(f"die reale  Güte nach t={t[i]} Sekunden {vreal2:.5f}")
-    print(f"die ideale Güte nach t={t[i]} Sekunden {vid:.5f}")
+    print(f"die reale  Güte nach t={t[i]}s: v_re={vreal2:.3f}")
+    print(f"die ideale Güte nach t={t[i]}s: v_id= {vid:.3f}")
     print(f"die Abweichung beträgt {p*100:.2f}% vom Idealwert")
     print("\n")
     i=i+10
@@ -118,8 +127,8 @@ params3, covariance_matrix3 = np.polyfit(1/T1, np.log(p1/p0), deg=1, cov=True)
 #Unsicherheit der Regression
 errors = np.sqrt(np.diag(covariance_matrix3))
 print("Die Parameter der Regression für p1:")
-print('a1 = {:.3f} ± {:.4f}'.format(params3[0], errors[0]))
-print('b1 = {:.3f} ± {:.4f}'.format(params3[1], errors[1]))
+print('a1 = {:.3f} ± {:.3f}'.format(params3[0], errors[0]))
+print('b1 = {:.3f} ± {:.3f}'.format(params3[1], errors[1]))
 print("\n")
 
 #Plotten der Regression
@@ -147,8 +156,8 @@ params4, covariance_matrix4 = np.polyfit(1/T2, np.log(p2/p0), deg=1, cov=True)
 #Unsicherheit für Regression
 errors = np.sqrt(np.diag(covariance_matrix4))
 print("Die Parameter der Regression für p2:")
-print('a2 = {:.3f} ± {:.4f}'.format(params4[0], errors[0]))
-print('b2 = {:.3f} ± {:.4f}'.format(params4[1], errors[1]))
+print('a2 = {:.3f} ± {:.3f}'.format(params4[0], errors[0]))
+print('b2 = {:.3f} ± {:.3f}'.format(params4[1], errors[1]))
 print("\n")
 
 #Plotten der Regression
@@ -172,8 +181,8 @@ plt.savefig('Druckverlaeufe.pdf')
 #Berechnung der Verdampfungswärme
 L1=-params3[0]*8.3144621 #hier am besten R einfügen
 L2=-params4[0]*8.3144621 #Einheit: [R]=J/(K*mol)
-print(f"Aus Messreihe 1 folgt: \n L={L1} \n")
-print(f"Aus Messreihe 2 folgt: \n L={L2}\n\n")
+print(f"Aus Messreihe 1 folgt: \n L={L1:.3f} \n")
+print(f"Aus Messreihe 2 folgt: \n L={L2:.3f}\n\n")
 
 #Brechnung des Massendurchsatzes
 # dQ/dt=L*dm/dt
@@ -184,8 +193,8 @@ print(f"Aus Messreihe 2 folgt: \n L={L2}\n\n")
 molmass=120.91 #g/mol
 i=1
 while(i<40):
-    massdu=-((a1+a2)*difT2.subs(x, i))/L1 #difQ2 #!!!warum difT2 und L2???
-    print(f"Der Massendruchsatz nach t={i}s ist: dm/dt={massdu:.5f}mol/s={massdu*molmass:.5f}g/s")
+    massdu=-((a1+a2)*difT2(i))/L1 #difQ2 #!!!warum difT2 und L2???
+    print(f"Der Massendruchsatz nach t={i}s ist: dm/dt=({massdu:.5f})mol/s=({massdu*molmass:.4f})g/s")
     i=i+10
 print("\n")
 #################################################################################
@@ -193,17 +202,27 @@ print("\n")
 
 #f) Die mechanische Leistung des Kompressors
 
+#Unsicherheit der Temperatur 
+T1err=np.array([h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h])
+T2err=np.array([h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h])
+i=0
+while(i<35):
+    #T1err[i]=ufloat(T1[i], 0.1)
+    T2err[i]=ufloat(T2[i], 0.1)
+    i=i+1
+
+#Berechnung der mechanischen Leistung
 i=1
 k=1.14
 rho0=5.51 * 10**3 #g/m^3
 T0=273.15 #K
 
 while(i<40):
-    rho=(rho0*T0*p1[i])/(T2[i]*p0) #g/m^3
+    rho=(rho0*T0*p1[i])/(T2err[i]*p0) #g/m^3
     Nmech1=(1/(k-1))
     Nmech2=Nmech1*(p2[i]*(p1[i]/p2[i])**(1/k)-p1[i]) #pa
     Nmech3=Nmech2*(1/rho) #pa*m^3/g
-    Nmech4=Nmech3*((a1+a2)/L1)*difT1.subs(x, i)*molmass #W
+    Nmech4=Nmech3*((a1+a2)/L1)*difT1(i)*molmass #W
     #print(rho*10**(-3))#kg/m^3
-    print(f"die mechanische Leistung nach t={t[i]} Sekunden: Nmech={Nmech4:.5f}W")
+    print(f"die mechanische Leistung nach t={t[i]} Sekunden: Nmech=({Nmech4:.5f})W")
     i=i+10
