44aaad7fbd88b1332cd0dc1a480f465b7429c52f
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2020-11-11 19:17:32 +0100

    Erweiterung Theorie, Erzeugung von 3 Graphen mit schwingungen.py

---
+++ V106/schwingungen.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy
+
+x=np.linspace(0, 4*np.pi)
+#plt.figure("""first figure""")
+plt.subplot(2, 1, 1)
+plt.plot(x, np.sin(x), color='red', label='Pendel1')
+plt.xlabel(r"$x$")
+plt.ylabel(r"$t$")
+plt.legend(loc="best")
+
+plt.subplot(2, 1, 2)
+plt.plot(x, np.sin(x), color='blue', label='Pendel2')
+plt.xlabel(r"$x$")
+plt.ylabel(r"$t$")
+plt.legend(loc="best")
+
+plt.savefig('gleichsinnig.pdf')
+
+plt.figure("""first figure""")
+plt.subplot(2, 1, 1)
+plt.plot(x, np.sin(x), color='red', label='Pendel1')
+plt.xlabel(r"$t$")
+plt.ylabel(r"$x$")
+plt.legend(loc="best")
+
+plt.subplot(2, 1, 2)
+plt.plot(x, -np.sin(x), color='blue', label='Pendel2')
+plt.xlabel(r"$t$")
+plt.ylabel(r"$x$")
+plt.legend(loc="best")
+
+plt.savefig('gegensinnig.pdf')
+
+plt.figure("""second figure""")
+x=np.linspace(0, 12*np.pi)
+plt.subplot(2, 1, 1)
+plt.plot(x, np.cos(2*np.pi*x)*np.cos(2*np.pi*x), color='red', label='Pendel1')
+plt.xlabel(r"$x$")
+plt.ylabel(r"$t$")
+plt.legend(loc="best")
+
+plt.subplot(2, 1, 2)
+plt.plot(x, -np.sin(2*np.pi*x)*np.cos(2*np.pi*x), color='blue', label='Pendel2')
+plt.xlabel(r"$t$")
+plt.ylabel(r"$x$")
+plt.legend(loc="best")
+
+plt.savefig('gekoppelt.pdf')
