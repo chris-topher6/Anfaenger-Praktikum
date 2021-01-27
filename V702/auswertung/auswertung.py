f77afc857bbba9c6506bb2e1cfb89e42d48a5643
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2021-01-27 23:36:09 +0100

    Aufgabenteil a) abgeschlossen, Aufgabenteil b) begonnen

--- V702/auswertung/auswertung.py
+++ V702/auswertung/auswertung.py
@@ -26,7 +26,6 @@ nulleffekt = nulleffekt/10
 
 #Berücksichtige Nulleffekt
 Nvanadium = impvanadium - np.mean(nulleffekt)
-Nrhodium = imprhodium - np.mean(nulleffekt)
 
 #fitte log(zerfallsgesetz)
 params1, covariance_matrix1 = np.polyfit(unp.nominal_values(tvanadium), np.log(unp.nominal_values(Nvanadium)), deg = 1, cov = True)
@@ -34,7 +33,7 @@ params1, covariance_matrix1 = np.polyfit(unp.nominal_values(tvanadium), np.log(u
 x_plot1 = np.linspace(0,1320)
 
 #Plotte Nvanadium
-plt.errorbar(unp.nominal_values(tvanadium), np.log(unp.nominal_values(Nvanadium)), xerr = unp.std_devs(tvanadium), yerr = np.log(unp.std_devs(Nvanadium)), fmt = "x", ecolor = "black", label="Messung")
+plt.errorbar(unp.nominal_values(tvanadium), np.log(unp.nominal_values(Nvanadium)), xerr = unp.std_devs(tvanadium), yerr = np.log(unp.std_devs(Nvanadium)), fmt = "x", ecolor = "black", label="Messung Vanadium")
 plt.plot(x_plot1, params1[0]*x_plot1+params1[1], label="Regression" )
 plt.ylabel("ln(N)")
 plt.xlabel("t [s]")
@@ -42,16 +41,48 @@ plt.legend()
 plt.savefig("PlotVanadiumLinLog1.pdf")
 plt.clf()
 #Angabe der Zerfallszeit(reziproke Zerfallskonstante) nach erster Regression
+print("Die Zerfallskonstante Lambda der ersten Regression von Vanadium beträgt: ", params1[0])
 zerfallszeit1 = 1/(params1[0] * (-1))
-print("Die Zerfallszeit beträgt: ", zerfallszeit1)
+print("Die Zerfallszeit der ersten Regression von Vanadium beträgt: ", zerfallszeit1)
 #Bestimmung der Halbwertszeit
-halbwertszeit = np.log(2/(params1[0] * (-1)))
-print("Die Halbwertszeit beträgt: ", halbwertszeit)
+halbwertszeit1 = np.log(2)/(params1[0] * (-1))
+print("Die Halbwertszeit der ersten Regression von Vanadium beträgt: ", halbwertszeit1)
 #Literaturangabe Wikipedia: 52 Vanadium 3.743min, ungefähr 220s
-#Plotte Nvanadium, beachte nur Werte bis 440s (zwischen Messpunkt t=420s und t= 450s)
-x_plot2 = np.linspace(0, 420)
+#Plotte Nvanadium, beachte nur Werte bis 460s (nächster Messpunkt bei t=450s)
+x_plot2 = np.linspace(0, 450)
 #tvanadium und Nvanadium verringern auf oben genannte Messwerte
-tvanadium2 = tvanadium[:14]
-Nvanadium2 = Nvanadium[:14]
-#params2, covariance_matrix2 = np.polyfit()
-#params2, covariance_matrix2 = np.polyfit(unp.nominal_values)
+tvanadium2 = tvanadium[:15]
+Nvanadium2 = Nvanadium[:15]
+#log(zerfallsgesetz) auf alles unter der doppelten Halbwertszeit fitten
+params2, covariance_matrix2 = np.polyfit(unp.nominal_values(tvanadium2), np.log(unp.nominal_values(Nvanadium2)), deg = 1, cov = True)
+#Plotten
+plt.errorbar(unp.nominal_values(tvanadium2), np.log(unp.nominal_values(Nvanadium2)), xerr = unp.std_devs(tvanadium2), yerr = np.log(unp.std_devs(Nvanadium2)), fmt = "x", ecolor = "black", label="Messung Vanadium")
+plt.plot(x_plot2, params2[0]*x_plot2+params2[1], label="Regression")
+plt.ylabel("ln(N)")
+plt.xlabel("t [s]")
+plt.legend()
+plt.savefig("PlotVanadiumLinLog2.pdf")
+plt.clf()
+print("Die Zerfallskonstante Lambda der zweiten Regression von Vanadium beträgt: ", params2[0])
+zerfallszeit2 = 1/(params2[0]*(-1))
+print("Die Zerfallszeit der zweiten Regression von Vanadium beträgt: ",zerfallszeit2)
+halbwertszeit2 = np.log(2)/(params2[0]*(-1))
+print("Die Halbwertszeit der zweiten Regression von Vanadium beträgt: ", halbwertszeit2)
+#Nulleffekt von 30s auf 15s Messintervalle bringen
+deltatnulleffekt = deltatnulleffekt/2
+nulleffekt = nulleffekt/2
+#Nulleffekt berücksichtigen
+Nrhodium = imprhodium - np.mean(nulleffekt)
+#plotte Rhodium
+plt.errorbar(unp.nominal_values(trhodium), np.log(unp.nominal_values(Nrhodium)), xerr = unp.std_devs(trhodium), yerr = np.log(unp.std_devs(Nrhodium)), fmt = "x", ecolor = "black", label = "Messung Rhodium")
+#Fitte Funktion an den Langlebigen Teil des Zerfalls
+#trhdodiumlanglebig = trhodium
+params3, covariance_matrix3 = np.polyfit(unp.nominal_values(trhodiumlanglebig), unp.nominal_values(Nrhodiumlanglebig), deg = 1, cov = True)
+x_plot3 = np.linspace(220, 660)
+plt.plot(x_plot3, params3[0]*x_plot3+params3[1], label = "Langlebige Regression")
+plt.ylabel("log(N)")
+plt.xlabel("t [s]")
+plt.legend()
+plt.savefig("PlotRhodiumLinLog1.pdf")
+plt.clf()
+print(trhodium())
