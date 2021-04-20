84925f033dff2d6768d2ed03ffbf4ad14e244616
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2021-04-20 09:32:13 +0200

    erster plot

--- V603/auswertung/auswertung.py
+++ V603/auswertung/auswertung.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+import scipy.constants as const
+from scipy.stats import sem
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+a, N  = np.genfromtxt('EmissionCu.dat', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+#grad in rad
+a     = a*np.pi/180
+
+#gerade für die Plots
+def gerade(x, m, b):
+    return m*x+b
+
+############################################################################################################
+#Aufgabe1
+print()
+print("Aufgabe 1")
+
+plt.figure()#Plot
+x=np.linspace(np.min(a), np.max(a))
+plt.plot(a ,N, '.', label='Messdaten')
+plt.xlabel(r"$\alpha [rad]$")
+plt.ylabel(r"$N [Imp/s]$")
 plt.legend(loc='best')
-
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.tight_layout()
+plt.savefig('plot1.pdf')
