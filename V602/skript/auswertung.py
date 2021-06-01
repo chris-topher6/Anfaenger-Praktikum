f88f24230516ac8da92614ee8bc3c39f7c00188b
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2021-06-01 08:28:24 +0200

    kleine Änderung

--- V602/skript/auswertung.py
+++ V602/skript/auswertung.py
@@ -6,11 +6,3 @@ import tools
 import pint
 ureg = pint.UnitRegistry()
 ureg.setup_matplotlib()
-
-#from uncertainties import ufloat
-#from uncertainties import unumpy
-#from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-#from scipy.stats import sem
-#from scipy.optimize import curve_fit
-#from scipy import optimize
-#from scipy.signal import argrelextrema
