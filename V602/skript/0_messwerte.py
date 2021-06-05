02b570a85714878b3776657df2be5ec7863074f3
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2021-06-05 15:35:54 +0200

    für Tabellen

---
+++ V602/skript/0_messwerte.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from generate_table import generate_table
+
+names = ['Bragg', 'Emissionsspektrum'] + ['Zink', 'Gallium', 'Brom', 'Rubidium', 'Strontium', 'Zirkonium']
+
+data = {}
+
+for i, name in enumerate(names):
+    θ, N = np.genfromtxt(f'data/{name}.dat', unpack=True)
+    generate_table(f'table_mess_{name.lower()}', [[*i] for i in zip(θ, N)], col_fmt=[{'d': 1}, {'d': 0}])
