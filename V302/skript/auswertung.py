42140e0619f4e53191c7aa1579875082212d7a84
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2021-06-15 18:31:22 +0200

    Aufgabenteil e) programmiert

--- V302/skript/auswertung.py
+++ V302/skript/auswertung.py
@@ -119,41 +119,78 @@ print("Damit weicht der Wert um ", abweichung(49.82, Lxd1), "% von 49.82 mH ab")
 print("--------------------------------------------------------")
 
 #Aufgabenteil e)
+#Teile U_B durch U_S=10V
+Ue = Ue/10
+#Widerstand R2 in Ohm
+R2e = 1000
+#Kapazität C in Farad
+Ce = 992*10**(-9)
+#Berechne v0
+v01 = 1/(R2e*Ce)
+print("Die berechnete Frequenz bei der U_B verschwindet beträgt v_0 = ",v01, "Hz")
+
+#Definiere zu fittende Funktion U_Br/U_S
+def brückenspannung(omegae, a):
+    UbrUs = np.sqrt((1/9) * (((omegae/a)**2 -1)**2)/((1-(omegae/a)**2)**2 +9*(omegae/a)**2))
+    return(UbrUs)
+
+#Fitte Funktion an Messwerte aus e)
+v02, cov = curve_fit(brückenspannung, Fe, Ue)
+print("Die aus dem Fit ermittelte Frequenz v_0 beträgt: ",v02, "Hz")
+print("--------------------------------------------------------")
+
+#Funktion und Messdaten plotten
+x=np.linspace(Fe[0], Fe[20], 10000)
+y=brückenspannung(x, v02)
+
+plt.figure()
+plt.plot(Fe/v02, Ue, 'r+', label='Messdaten')
+plt.plot(x/v02, y, 'gray')
+plt.xscale('log')
+plt.xlabel(r'$v/v_0$')
+plt.ylabel(r'$U_{Br}/U_{S}$')
+plt.legend(loc='best')
+plt.savefig('build/plotE.pdf')
+
+#Berechnung des Klirrfaktors
+klirrfaktor = Ue[5]/brückenspannung(2,1)
+print("Der Klirrfaktor beträgt: ", klirrfaktor)
+print("--------------------------------------------------------")
 
 
 #Erzeugung der Latex-Tabellen (muss noch überarbeitet werden!!!)
 #Tabelle a
-tablea = np.transpose(np.array([R3a, R4a, Rxa, R2a]))
-headersa = ["{R_3}", "{R_4}", "{R_x}", "{R_2}"]
-print("Tabelle a: ")
-print("--------------------------------------------------------")
-print(tabulate(tablea,  headers= headersa, tablefmt="latex_raw"))
-print("--------------------------------------------------------")
+#tablea = np.transpose(np.array([R3a, R4a, Rxa, R2a]))
+#headersa = ["{R_3}", "{R_4}", "{R_x}", "{R_2}"]
+#print("Tabelle a: ")
+#print("--------------------------------------------------------")
+#print(tabulate(tablea,  headers= headersa, tablefmt="latex_raw"))
+#print("--------------------------------------------------------")
 #Tabelle b
-tableb = np.transpose(np.array([R3b, R4b, R2b, C2b, Rxb, Cxb]))
-headersb = ["{R_3}", "{R_4}", "{R_2}", "{C_2}", "{R_x}", "{C_x}"]
-print("Tabelle b: ")
-print("--------------------------------------------------------")
-print(tabulate(tableb,  headers= headersb, tablefmt="latex_raw"))
-print("--------------------------------------------------------")
+#tableb = np.transpose(np.array([R3b, R4b, R2b, C2b, Rxb, Cxb]))
+#headersb = ["{R_3}", "{R_4}", "{R_2}", "{C_2}", "{R_x}", "{C_x}"]
+#print("Tabelle b: ")
+#print("--------------------------------------------------------")
+#print(tabulate(tableb,  headers= headersb, tablefmt="latex_raw"))
+#print("--------------------------------------------------------")
 #Tabelle c
-tablec = np.transpose(np.array([R3c, R4c, R2c, L2c, Rxc, Lxc]))
-headersc = ["{R_3}", "{R_4}", "{R_2}", "{L_2}", "{R_x}", "{L_x}"]
-print("Tabelle c: ")
-print("--------------------------------------------------------")
-print(tabulate(tablec,  headers= headersc, tablefmt="latex_raw"))
-print("--------------------------------------------------------")
+#tablec = np.transpose(np.array([R3c, R4c, R2c, L2c, Rxc, Lxc]))
+#headersc = ["{R_3}", "{R_4}", "{R_2}", "{L_2}", "{R_x}", "{L_x}"]
+#print("Tabelle c: ")
+#print("--------------------------------------------------------")
+#print(tabulate(tablec,  headers= headersc, tablefmt="latex_raw"))
+#print("--------------------------------------------------------")
 #Tabelle d
-tabled = np.transpose(np.array([R3d, R4d, R2d, Cd, Rx, Lx]))
-headersd = ["{R_3}", "{R_4}", "{R_2}", "{C_d}", "{R_x}", "{L_x}"]
-print("Tabelle d: ")
-print("--------------------------------------------------------")
-print(tabulate(tabled,  headers= headersd, tablefmt="latex_raw"))
-print("--------------------------------------------------------")
+#tabled = np.transpose(np.array([R3d, R4d, R2d, Cd, Rx, Lx]))
+#headersd = ["{R_3}", "{R_4}", "{R_2}", "{C_d}", "{R_x}", "{L_x}"]
+#print("Tabelle d: ")
+#print("--------------------------------------------------------")
+#print(tabulate(tabled,  headers= headersd, tablefmt="latex_raw"))
+#print("--------------------------------------------------------")
 #Tabelle e
-tablee = np.transpose(np.array([Fe, Ue]))
-headerse = ["{f [Hz]}", "{U [V]}"]
-print("Tabelle e: ")
-print("--------------------------------------------------------")
-print(tabulate(tablee,  headers= headerse, tablefmt="latex_raw"))
-print("--------------------------------------------------------")
+#tablee = np.transpose(np.array([Fe, Ue]))
+#headerse = ["{f [Hz]}", "{U [V]}"]
+#print("Tabelle e: ")
+#print("--------------------------------------------------------")
+#print(tabulate(tablee,  headers= headerse, tablefmt="latex_raw"))
+#print("--------------------------------------------------------")
