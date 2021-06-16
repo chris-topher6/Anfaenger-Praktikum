54300fbf12cf9fc1f590c21b56ab8413ed2ec345
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2021-06-16 22:23:42 +0200

    soweit fertig

--- V204/skript/statisch.py
+++ V204/skript/statisch.py
@@ -5,24 +5,24 @@ import sympy
 from sympy import *
 from uncertainties import ufloat
 
-I, T1, T4, T5, T8, T1, T2, T3, T6, T7 = np.genfromtxt('data/Temperatur_statisch.txt', unpack=True)
+I, T1, T2, T3, T4, T5, T6, T7, T8 = np.genfromtxt('data/stat.dat', unpack=True)
 #hier bei der Reihnfolge unbedingt überpüfen!!!
 
 t=5*I
-plt.plot(t, T1, 'k.', label='Messing breit')
-plt.plot(t, T4, 'g.', label='Messing schmal')
-plt.plot(t, T5, 'b.', label='Aluminium')
-plt.plot(t, T8, 'r.', label='Edelstahl')
-plt.xlabel('Zeit/$s$')
-plt.ylabel('Temperatur/ °C')
+plt.plot(t, T1, 'k.', markersize=1, label='Messing breit')
+plt.plot(t, T4, 'g.', markersize=1, label='Messing schmal')
+plt.plot(t, T5, 'b.', markersize=1, label='Aluminium')
+plt.plot(t, T8, 'r.', markersize=1, label='Edelstahl')
+plt.xlabel('t/$s$')
+plt.ylabel('T/ °C')
 plt.legend(loc='best')
 plt.savefig('build/plot1.pdf')
 
 plt.figure()
-plt.plot(t, T2-T1, 'k.', label='Messing breit')
-plt.plot(t, T7-T8, 'b.', label='Edelstahl')
-plt.xlabel('Zeit/$s$')
-plt.ylabel('$\Delta$Temperatur/ °C')
+plt.plot(t, T2-T1, 'k.', markersize=1, label='Messing breit')
+plt.plot(t, T7-T8, 'b.', markersize=1, label='Edelstahl')
+plt.xlabel('t/$s$')
+plt.ylabel('$\Delta T_{St}$/ °C')
 plt.legend(loc='best')
 plt.savefig('build/plot2.pdf')
 
@@ -45,7 +45,7 @@ c_edelstahl=400
 A_edelstahl=0.012*0.004
 
 #zeitpunkte: 100s, 200s, 300s, 400s, 500s
-i=np.array([19,39,59,79,99])
+i=np.array([19,39,59,79,99])                #was sind das für komische Zeiten?
 j=0
 while j < 5:
     dQ=np.array([-k_messing*A_messingb*(T2[i[j]]-T1[i[j]])/d_x, -k_messing*A_messings*(T3[i[j]]-T4[i[j]])/d_x,-k_aluminium*A_aluminium*(T6[i[j]]-T5[i[j]])/d_x,  -k_edelstahl*A_edelstahl*(T7[i[j]]-T8[i[j]])/d_x])
@@ -54,10 +54,12 @@ while j < 5:
 
 j=0
 while j<5:
-    print(T2[i[j]]-T1[i[j]],T3[i[j]]-T4[i[j]],T6[i[j]]-T5[i[j]],T7[i[j]]-T8[i[j]])  
+    print(f"{T2[i[j]]-T1[i[j]]:.3},{T3[i[j]]-T4[i[j]]:.3},{T6[i[j]]-T5[i[j]]:.3},{T7[i[j]]-T8[i[j]]:.3}")  
     j=j+1
 
-print(T1[19]+273.15,T1[39]+273.15,T1[59]+273.15,T1[79]+273.15,T1[99]+273.15)
-print(T4[19]+273.15,T4[39]+273.15,T4[59]+273.15,T4[79]+273.15,T4[99]+273.15)
-print(T5[19]+273.15,T5[39]+273.15,T5[59]+273.15,T5[79]+273.15,T5[99]+273.15)
-print(T8[19]+273.15,T8[39]+273.15,T8[59]+273.15,T8[79]+273.15,T8[99]+273.15)
+print()
+print(f"T1: {T1[19]+273.15:.4},{T1[39]+273.15:.4},{T1[59]+273.15:.4},{T1[79]+273.15:.4},{T1[99]+273.15:.4}")
+print(f"T4: {T4[19]+273.15:.4},{T4[39]+273.15:.4},{T4[59]+273.15:.4},{T4[79]+273.15:.4},{T4[99]+273.15:.4}")
+print(f"T5: {T5[19]+273.15:.4},{T5[39]+273.15:.4},{T5[59]+273.15:.4},{T5[79]+273.15:.4},{T5[99]+273.15:.4}")
+print(f"T8: {T8[19]+273.15:.4},{T8[39]+273.15:.4},{T8[59]+273.15:.4},{T8[79]+273.15:.4},{T8[99]+273.15:.4}")
+print()
