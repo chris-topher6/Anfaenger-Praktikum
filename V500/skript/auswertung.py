230a37fb1962cb663616c72c2047edfc68a4b27e
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2021-06-30 12:51:35 +0200

    Python für b) fertig

--- V500/skript/auswertung.py
+++ V500/skript/auswertung.py
@@ -40,13 +40,13 @@ params = {}
 errors = {}
 print("Aufgabenteil a):")
 
-#Fit an Grün (1)
+#Fit an Grün (1) (546nm)
 params[1], cov1 = np.polyfit(U_grün, np.sqrt(I_grün), deg=1, cov=True)
 errors[1] = np.sqrt(np.diag(cov1))
 #x-Werte für Plot der Regressionsgeraden
 x1 = np.linspace(-1, 2.5, num=35)
 #Plot der Regressionsgeraden
-plt.plot(x1, params[1][0]*x1+params[1][1], "r", label="Lineare Regression", linewidth=1)
+plt.plot(x1, params[1][0]*x1+params[1][1], "r", label="Lineare Regression", linewidth=0.8)
 #Plot der Messwerte
 plt.plot(U_grün, np.sqrt(I_grün),  "k.", label="Grün", linewidth=1)
 plt.xlabel(r"$U_{Gruen}/V $")
@@ -55,13 +55,13 @@ plt.legend()
 plt.savefig("build/plotgrün.pdf")
 plt.clf()
 
-#Fit an Blaugrün (2)
+#Fit an Blaugrün (2) (492nm)
 params[2], cov2 = np.polyfit(U_blaugrün, np.sqrt(I_blaugrün), deg=1, cov=True)
 errors[2] = np.sqrt(np.diag(cov2))
 #x-Werte für Plot der Regressionsgeraden
 x2 = np.linspace(-1, 2, num=30)
 #Plot der Regressionsgeraden
-plt.plot(x2, params[2][0]*x2+params[2][1], "r", label="Lineare Regression", linewidth=1)
+plt.plot(x2, params[2][0]*x2+params[2][1], "r", label="Lineare Regression", linewidth=0.8)
 #Plot der Messwerte
 plt.plot(U_blaugrün, np.sqrt(I_blaugrün),  "k.", label="Blaugrün", linewidth=1)
 plt.xlabel(r"$U_{Blaugruen}/V $")
@@ -70,13 +70,13 @@ plt.legend()
 plt.savefig("build/plotblaugrün.pdf")
 plt.clf()
 
-#Fit an Blau (3)
+#Fit an Blau (3) (435nm)
 params[3], cov3 = np.polyfit(U_blau, np.sqrt(I_blau), deg=1, cov=True)
 errors[3] = np.sqrt(np.diag(cov3))
 #x-Werte für Plot der Regressionsgeraden
 x3 = np.linspace(-1.4, 2, num=34)
 #Plot der Regressionsgeraden
-plt.plot(x3, params[3][0]*x3+params[3][1], "r", label="Lineare Regression", linewidth=1)
+plt.plot(x3, params[3][0]*x3+params[3][1], "r", label="Lineare Regression", linewidth=0.8)
 #Plot der Messwerte
 plt.plot(U_blau, np.sqrt(I_blau),  "k.", label="Blau", linewidth=1)
 plt.xlabel(r"$U_{Blau}/V $")
@@ -85,13 +85,13 @@ plt.legend()
 plt.savefig("build/plotblau.pdf")
 plt.clf()
 
-#Fit an Gelb (4)
-params[4], cov4 = np.polyfit(U_gelb[0:12], np.sqrt(I_gelb[0:12]), deg=1, cov=True)
+#Fit an Gelb (4) (577nm)
+params[4], cov4 = np.polyfit(U_gelb[0:18], np.sqrt(I_gelb[0:18]), deg=1, cov=True)
 errors[4] = np.sqrt(np.diag(cov4))
 #x-Werte für Plot der Regressionsgeraden
 x4 = np.linspace(-0.2, 20, num=21)
 #Plot der Regressionsgeraden
-plt.plot(x4, params[4][0]*x4+params[4][1], "r", label="Lineare Regression", linewidth=1)
+plt.plot(x4, params[4][0]*x4+params[4][1], "r", label="Lineare Regression", linewidth=0.8)
 #Plot der Messwerte
 plt.plot(U_gelb, np.sqrt(I_gelb),  "k.", label="Gelb", linewidth=1)
 plt.xlabel(r"$U_{Gelb}/V $")
@@ -117,34 +117,38 @@ for i in range(1,5):
     print("")
 
 #b)
-ug_l = np.array([ug[3].n, ug[2].n, ug[1].n, ug[4].n])
-lambda_ug = np.array([435, 492, 546, 577])#in nm
-lambda_ug = lambda_ug*10**(-9)#in m
-#Plot der Messwerte
-plt.plot(const.c/lambda_ug, ug_l, "k.", label="Messwerte", linewidth=1)
-plt.xlabel(r"$f/Hz$")
-plt.ylabel(r"$U_g/V$")
-plt.legend()
-plt.savefig("build/plotb1.pdf")
-plt.clf()
+ug_b = np.array([ug[1].n, ug[2].n, ug[3].n, ug[4].n])
+lambda_b = np.array([546, 492, 435, 577]) #in nm
+lambda_b = 10**(-9)*lambda_b #in m
 #Entferne Gelb
-ug_l = ug_l[0:3]
-lambda_ug = lambda_ug[0:3]
-#Fit
-params_ug, cov5 = np.polyfit((const.c/lambda_ug), ug_l, deg=1, cov=True)
+ug_bg = ug_b[0:3]
+lambda_bg = lambda_b[0:3]
+#Fit an alle Daten
+params_ug, cov5 = np.polyfit((const.c/lambda_b), ug_b, deg=1, cov=True)
 errors_ug = np.sqrt(np.diag(cov5))
+#Fit ohne Gelb
+params_ugg, cov6 = np.polyfit((const.c/lambda_bg), ug_bg, deg=1, cov=True)
+errors_ugg = np.sqrt(np.diag(cov6))
 print("Aufgabenteil b:")
+print("")
+print("Fit an alle Daten:")
 print("h/e0 = ", params_ug[0])
-print("Ak = ", params_ug[1]*const.e)
-#Plot des Fits und der Messwerte
-plt.plot(lambda_ug, (params_ug[0]*ug_l+params_ug[1]), "r", label="Fit", linewidth=1)
-plt.plot(const.c/lambda_ug, ug_l, "k.", label="Messwerte", linewidth=1)
+print("Ak = ", (-1)*params_ug[1]*const.e)
+print("")
+print("Fit an Daten ohne Gelb = 577nm:")
+#x-Koordinaten vom kleinsten lambda bis zum größten lambda
+xb = np.linspace(const.c/lambda_b[2],const.c/lambda_b[3])
+xbg = np.linspace(const.c/lambda_bg[2], const.c/lambda_bg[0])
+#Plot der Fits und der Messwerte
+plt.plot(xb, params_ug[0]*xb+params_ug[1], "r", label="Fit 1", linewidth=0.8)
+plt.plot(xbg, params_ugg[0]*xbg+params_ugg[1], "b", label="Fit 2", linewidth=0.8)
+plt.plot(const.c/lambda_b, ug_b, "k.", label="Messwerte", linewidth=1)
 plt.xlabel(r"$f/Hz$")
 plt.ylabel(r"$U_g/V$")
 plt.legend()
-plt.savefig("build/plotb2.pdf")
+plt.savefig("build/plotb.pdf")
 plt.clf()
-
+#
 #c)
 plt.plot(U_gelb, I_gelb, "k.", label="Gelb", linewidth=1)
 plt.xlabel(r"$U_{Gelb}/V$")
