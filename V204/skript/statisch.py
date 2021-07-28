da2c8131109b687d018315ec37e00846b51afa3b
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2021-07-28 15:18:43 +0200

    fertig

--- V204/skript/statisch.py
+++ V204/skript/statisch.py
@@ -30,18 +30,22 @@ np.savetxt('build/intervalla.txt',np.column_stack( [t, T1, T2, T3, T4, T5, T6, T
 
 np.savetxt('build/tabelle1.txt',np.column_stack( [ T1[3500],T4[3500],T5[3500],T8[3500] ] ), fmt='%10.2f' , delimiter='&',header='T(700s) T1 T4 T5 T8', newline='\\\\\n' )
 d_x=0.03
+
 k_messing=120 #W/mk
+k_aluminium=235
+k_edelstahl=21
+
 rho_messing=8520 #kg/m^3
+rho_aluminium=2800 #kg/m^3
+rho_edelstahl=8000 #kg/m^3
+
 c_messing=377
+c_aluminium=897
+c_edelstahl=400
+
 A_messingb=0.012*0.004
 A_messings=0.007*0.004
-k_aluminium=235
-rho_aluminium=2800 #kg/m^3
-c_aluminium=897
 A_aluminium=0.012*0.004
-k_edelstahl=21
-rho_edelstahl=8000 #kg/m^3
-c_edelstahl=400
 A_edelstahl=0.012*0.004
 
 #zeitpunkte: 100s, 200s, 300s, 400s, 500s
@@ -50,11 +54,13 @@ i=np.array([499, 999, 1499, 1999, 2499])
 j=0
 print('dQ:')
 while j < 5:
-    dQ=np.array([-k_messing*A_messingb*(T2[i[j]]-T1[i[j]])/d_x, -k_messing*A_messings*(T3[i[j]]-T4[i[j]])/d_x,-k_aluminium*A_aluminium*(T6[i[j]]-T5[i[j]])/d_x,  -k_edelstahl*A_edelstahl*(T7[i[j]]-T8[i[j]])/d_x])
+    #dQ/dt=-k*A*(Ti-Tj)/dx
+    dQ=np.array([-k_messing*A_messingb*(T1[i[j]]-T2[i[j]])/d_x, -k_messing*A_messings*(T3[i[j]]-T4[i[j]])/d_x,-k_aluminium*A_aluminium*(T5[i[j]]-T6[i[j]])/d_x,  -k_edelstahl*A_edelstahl*(T7[i[j]]-T8[i[j]])/d_x])
     j=j+1
     print(dQ)
 
 j=0
+print('dT')
 while j<5:
     print(f"{T2[i[j]]-T1[i[j]]:.3},     {T3[i[j]]-T4[i[j]]:.3},     {T6[i[j]]-T5[i[j]]:.3},     {T7[i[j]]-T8[i[j]]:.3}")  
     j=j+1
